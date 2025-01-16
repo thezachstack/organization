7f824e598775328c8cbd447308bddcaf43daa8c7
Zachary Sales <57190746+zacharyzero@users.noreply.github.com>
2025-01-16 14:54:38 -0800

    Create create_a_grade_book.py
    My solution for Codecademy's create a gradebook project for Python.
    
    https://www.codecademy.com/projects/practice/python-gradebook

---
+++ create_a_grade_book.py
@@ -0,0 +1,39 @@
+last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
+
+# Your code below: 
+subjects = [
+  "physics",
+  "calculus",
+  "poetry",
+  "history",
+]
+
+grades = [
+  "98",
+  "97",
+  "85",
+  "88",
+]
+
+gradebook= [
+    [subjects[0], grades[0]],
+    [subjects[1], grades[1]],
+    [subjects[2], grades[2]],
+    [subjects[3], grades[3]],
+]
+
+gradebook.append(["computer science", "100"])
+gradebook.append(["visual arts", "93"])
+
+gradebook[-1][1] = "98"
+
+gradebook.remove(["poetry", "85"])
+gradebook.append(["poetry", "Pass"])
+
+full_gradebook = last_semester_gradebook + gradebook
+
+print("This is a list of current grades")
+print(f"\n{gradebook}")
+
+print("\nThis is a list of all grades")
+print(f"\n{full_gradebook}")
