521d2eb5334d5696972fcf93c03f9ec4bffce326
Zachary Sales <57190746+zacharyzero@users.noreply.github.com>
2025-01-31 13:56:29 -0800

    Update create_a_grade_book.py

--- create_a_grade_book.py
+++ create_a_grade_book.py
@@ -1,7 +1,9 @@
 #This scrips is my solution for Codecademy's gradebook project using Python
+
+#This sets the preassigned values to the last_semester_gradebook variable based on the contents of the Codecademy course
 last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
 
-# Your code below: 
+# This is a list of subjects
 subjects = [
   "physics",
   "calculus",
@@ -9,6 +11,7 @@ subjects = [
   "history",
 ]
 
+#This is a list of grades
 grades = [
   "98",
   "97",
@@ -16,6 +19,7 @@ grades = [
   "88",
 ]
 
+#The gradebook variable combines the subjects list and grades list
 gradebook= [
     [subjects[0], grades[0]],
     [subjects[1], grades[1]],
@@ -34,7 +38,9 @@ gradebook.append(["poetry", "Pass"])
 full_gradebook = last_semester_gradebook + gradebook
 
 print("This is a list of current grades:")
-print(f"\n{gradebook}")
+for subject, grade in gradebook:
+    print(f"{subject}: {grade}")
 
 print("\nThis is a list of all grades:")
-print(f"\n{full_gradebook}")
+for subject, grade in full_gradebook:
+    print(f"{subject}: {grade}")
