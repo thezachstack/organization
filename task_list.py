cfae4f9133f6007cf4709d5121e2fc9e94c875d4
Zachary Sales <57190746+zacharyzero@users.noreply.github.com>
2025-05-30 12:46:40 -0700

    Update task_list.py

--- task_list.py
+++ task_list.py
@@ -3,6 +3,8 @@
 #This is a list of my personal tasks
 tasks = [
     "Obtain Microsoft Certified Azure Fundamentals Certification",
+    "Obtain Microsoft Certified: Dynamics 365 Fundamentals (CRM)",
+    "ObtainMicrosoft Certified: Dynamics 365 Fundamentals (ERP)",
     "Obtain Amazon Web Services Certified Cloud Practitioner Certification",
     "Obtain Google Associate Cloud Engineer Certification",
     "Obtain Microsoft Certified Azure Artificial Intelligence Fundamentals Certification",
@@ -10,6 +12,7 @@ tasks = [
     "Obtain Splunk Core Certified User Certification",
     "Obtain Linux Plus Certification",
     "Obtain Security Onion Certified Professional Certification",
+    "Obtain Wireshark Certified Network Analyst (WCNA) Certification",
     "Obtain Kubernetes and Cloud Native Associate Certification",
     "Obtain Certified Security Operations Center Analyst Certification",
     "Obtain Docker Certified Associate Certification",
@@ -34,7 +37,7 @@ tasks = [
     "Obtain Cisco 300-725 Securing the Web with Cisco Secure Web Appliance Certification",
     "Obtain Cisco 300-730 Implementing Secure Solutions with Virtual Private Networks Certification",
     "Obtain Cisco 350-701 Implementing and Operating Cisco Security Core Technologies Certification",
-    "Obtain Cisco Certified Design Expert Certification",
+    "Obtain Cisco Certified Design Expert Certification\n"
 ]
 
 #This function is used to display the tasks in the list
@@ -42,7 +45,7 @@ def view_tasks():
   if not tasks:
     print("\nNo tasks available.")
   else:
-    print("\nYour current task list:")
+    print("\nYour current task list:\n")
     for i, task in enumerate(tasks, start=1):
       print(f"{i}. {task}")
 
